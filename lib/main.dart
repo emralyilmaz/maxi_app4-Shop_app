3a25ce97c4a635445901632702fcc9ae10f7121c
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-10-18 17:58:55 +0300

    186. Working on the 'Products'  Grid & Item Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app4_shop_app/screens/products_overview_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,26 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("MyShop"),
-      ),
-      body: Center(
-        child: Text("Let's build a shop!"),
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -5,7 +5,7 @@ class Product {
   final String title;
   final String description;
   final double price;
-  final String imaheUrl;
+  final String imageUrl;
   bool isFavorite;
 
   Product(
@@ -13,6 +13,6 @@ class Product {
       @required this.title,
       @required this.description,
       @required this.price,
-      @required this.imaheUrl,
+      @required this.imageUrl,
       this.isFavorite = false});
 }
