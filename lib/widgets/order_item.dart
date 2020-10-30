31b66f257430d753c542462ed22b8a96e3e9a1a6
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-10-30 11:00:04 +0300

    208. Adding an Orders Screen

---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:app4_shop_app/providers/orders.dart' as ord;
+import 'package:intl/intl.dart';
+
+class OrderItem extends StatelessWidget {
+  final ord.OrderItem order;
+
+  OrderItem(this.order);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text("\$${order.amount}"),
+            subtitle:
+                Text(DateFormat("dd mm yyyy hh:mm").format(order.dateTime)),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
