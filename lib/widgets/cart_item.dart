1a784fd3221dab09f6329619b2accbfa73da8ae3
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-10-31 23:08:19 +0300

    215. Showing Alert Dialogs

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -28,6 +28,29 @@ class CartItem extends StatelessWidget {
       ),
       direction: DismissDirection
           .endToStart, //sadece sagdan sola hareket etmesi sağlandı.
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text("Are you sure?"),
+                  content:
+                      Text("Do you want to remove the item from the cart?"),
+                  actions: [
+                    FlatButton(
+                      child: Text("No"),
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                      },
+                    ),
+                    FlatButton(
+                      child: Text("Yes"),
+                      onPressed: () {
+                        Navigator.of(context).pop(true);
+                      },
+                    )
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
