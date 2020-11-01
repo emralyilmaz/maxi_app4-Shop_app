8950755ec818c82a4327d5d011f0f57ef49c4fdf
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-11-01 19:38:40 +0300

    221. Multiline Inputs & Disposing Objects

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -8,6 +8,17 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    // Ekrandan ayrıldığında, focus nodes bellekte kalır ve yer kaplarlar.
+    // bu sebeple dispose ile elden cıkarma işlemi yapılmadır.
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,9 +38,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
               TextFormField(
                 decoration: InputDecoration(labelText: "Price"),
                 textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.number,
+                // keyboardType: TextInputType.number,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
                 focusNode: _priceFocusNode,
-              )
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Description"),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
+              ),
             ],
           ),
         ),
