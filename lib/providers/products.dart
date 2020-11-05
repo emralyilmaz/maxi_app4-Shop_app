c2a0a7d4fb49ee6fce3c4e04dde085038c87ebfa
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-11-05 18:39:49 +0300

    241. Showing a Loading Indicator

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -65,9 +65,9 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct(Product product) {
+  Future<void> addProduct(Product product) {
     const url = "https://shopapp-maxi.firebaseio.com/products.json";
-    http
+    return http
         .post(
       url,
       body: json.encode({
