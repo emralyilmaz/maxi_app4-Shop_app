8dbf377ff292eb911d668460c941d337ab339304
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-11-13 20:53:52 +0300

    265. Using the 'ProxyProvider' and Attaching the Token to Outgoing Http Requests

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -40,6 +40,8 @@ class Products with ChangeNotifier {
     // ),
   ];
   // var _showFavoritesOnly = false;
+  final String authToken;
+  Products(this.authToken, this._items);
 
   List<Product> get items {
     // if (_showFavoritesOnly) {
@@ -67,7 +69,8 @@ class Products with ChangeNotifier {
   // }
 
   Future<void> fetchAndSetProducts() async {
-    const url = 'https://shopapp-maxi.firebaseio.com/products.json';
+    final url =
+        'https://shopapp-maxi.firebaseio.com/products.json?auth=$authToken';
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
